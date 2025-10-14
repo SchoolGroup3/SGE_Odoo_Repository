292c963e710e5636c2eec9944c8ca8bb8075eea4
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-10-14 12:09:11 +0200

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Incidencias Reto 1',
+    'version': '1.1',
+    'category': 'Human Resources/Employees',
+    'summary': 'Incidencias reto 1',
+    'website': 'https://www.odoo.com/app/employees',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
